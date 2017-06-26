6eb80e850118cec3d939352706667e17f92d1204
Soham Chowdhury <chow.soham@gmail.com>
2017-06-26 11:42:48 +0530

    Add Emacs locals file

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (intero-targets "ariadne:lib" "katip:lib" "opaleye:lib" "tisch:lib")))
