d4f99076261e17489e6019cf4e51cee623c097c3
Soham Chowdhury <chow.soham@gmail.com>
2017-07-02 08:08:22 +0530

    Remove unused imports in Knot model

--- library/ariadne/Ariadne/Models/Knot.hs
+++ library/ariadne/Ariadne/Models/Knot.hs
@@ -20,33 +20,15 @@
 module Ariadne.Models.Knot where
 
 import           Control.Arrow              (returnA)
-import           Control.Monad.Catch        (MonadThrow)
-import           Control.Monad.IO.Class     (MonadIO, liftIO)
-import           Control.Monad.Logger
-import           Control.Monad.Reader
 import           Data.Aeson
-import qualified Data.ByteString            as BS
-import           Data.Tagged
-import           Data.Text                  (Text)
-import qualified Data.Text                  as T
-import qualified Data.Text.Encoding         as T
-import qualified Data.Text.Encoding.Error   as T
-import qualified Data.Text.Lazy             as TL
-import qualified Data.Text.Lazy.Encoding    as TL
 import           Data.Time.Clock            (UTCTime)
 import           Data.UUID
-import qualified Database.PostgreSQL.Simple as PGS
-import           GHC.TypeLits
 import           Edible
-import           Edible.Internal.Table
 
 import           Lib.Prelude                hiding (like)
 
 import           Ariadne.Ambiguous
 import           Ariadne.Database
-import qualified Ariadne.Logging            as L
-
-import qualified Network.Wreq               as W
 
 ----------
 -- KnotId
